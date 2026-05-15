86a6c80bf15cb3f07008f9715a156700842de10d
ever4st <71986795+ever4st@users.noreply.github.com>
2026-05-15 18:07:45 +0300

    Update imports in default.nix with new configurations
    Added new configuration files for audio, boot, hardware, network, nix, sddm, and system.

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -2,9 +2,15 @@
 
 {
   imports = [
-    ./amnezia.nix
-    ./postgres.nix
-    ./bluetooth.nix
+    ./audio.nix
+    ./boot.nix
+    ./hardware.nix
+    ./network.nix
+    ./nix.nix
+    ./sddm.nix
+    ./system.nix
+    ./amnezia.nix  # Твой VPN
+    ./db.nix       # Базы данных (Postgres/Sqlite)
     # Добавляй сюда новые файлы по мере создания
   ];
 }
