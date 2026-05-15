09d12376e545f93754dc22d58b67880e92dbb824
ever4st <71986795+ever4st@users.noreply.github.com>
2026-05-15 19:16:35 +0300

    Enhance theme configuration with new GTK and Qt settings
    Added additional themes and cursor settings for GTK and Qt applications.

--- modules/home-manager/theme.nix
+++ modules/home-manager/theme.nix
@@ -4,6 +4,10 @@
     nerd-fonts.jetbrains-mono
     nerd-fonts.symbols-only
     font-awesome
+    dracula-theme        # Основная тема GTK
+    layan-gtk-theme      # Пастельная альтернатива для разнообразия
+    papirus-icon-theme   # Иконки (лучше всего подходят под Dracula)
+    dracula-cursors      # Курсоры
   ];
 
   # Настройка GTK (тема Dracula)
@@ -21,6 +25,10 @@
       name = "Dracula-cursors";
       package = pkgs.dracula-theme;
     };
+    font = {
+      name = "JetBrainsMono Nerd Font";
+      size = 11;
+    };
   };
 
   # Чтобы Qt приложения выглядели как GTK (Dracula)
@@ -28,5 +36,9 @@
     enable = true;
     platformTheme.name = "gtk";
     style.name = "adwaita-dark";
+  };
+  home.sessionVariables = {
+    XCURSOR_SIZE = "24";
+    HYPRCURSOR_SIZE = "24";
   };
 }
