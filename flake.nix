1c98a4ab5df395fa951fda43a40d4aa6beb7a0f5
ever4st <71986795+ever4st@users.noreply.github.com>
2026-05-15 19:24:38 +0300

    Remove home-manager configuration
    Removed home-manager configuration from flake.nix.

--- flake.nix
+++ flake.nix
@@ -20,12 +20,6 @@
       specialArgs = { inherit inputs; };
       modules = [
         ./hosts/laptop/configuration.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.ever4pain = import ./modules/home-manager/default.nix;
-        }
       ];
     };
   };
