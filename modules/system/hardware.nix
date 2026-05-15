963ed8528d2c98f12ddc2849f4d1e1433ac5ffaf
ever4st <71986795+ever4st@users.noreply.github.com>
2026-05-15 18:13:04 +0300

    Add AMD GPU support in hardware configuration

---
+++ modules/system/hardware.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }: 
+
+{
+  # Поддержка AMD GPU
+  boot.initrd.kernelModules = [ "amdgpu" ];
+  services.xserver.videoDrivers = [ "amdgpu" ];
+  
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+}
