5c7c248cbee75d970eb877679ad5f65c49f00816
ever4st <71986795+ever4st@users.noreply.github.com>
2026-05-15 19:39:56 +0300

    Configure Amnezia VPN and enable traffic forwarding
    Add Amnezia VPN package and enable IP forwarding

--- modules/system/amnezia.nix
+++ modules/system/amnezia.nix
@@ -1,1 +1,7 @@
-
+{ pkgs, ... }: {
+  environment.systemPackages = [ pkgs.amnezia-vpn ];
+  
+  # Amnezia часто использует туннели, разрешаем форвардинг трафика
+  boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
+  boot.kernel.sysctl."net.ipv6.conf.all.forwarding" = 1;
+}
