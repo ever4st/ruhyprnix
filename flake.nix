d1f4ee1dcd6d9cbe8e40f96a24d325b3f2b6b3d3
ever4st <71986795+ever4st@users.noreply.github.com>
2026-05-15 15:53:11 +0300

    Create flake.nix for NixOS configuration
    Added Nix flake configuration for my laptop with Home Manager and Hyprland.

--- flake.nix
+++ flake.nix
@@ -1,1 +1,28 @@
+{
+  description = "Мой флейк nix os на русском";
 
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    
+    # Подключаем базу HydeNix для визуала
+    hyprland.url = "github:hyprwm/Hyprland";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations.my-laptop = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./hosts/laptop/configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.ever4pain = import ./modules/home/home.nix;
+        }
+      ];
+    };
+  };
+}
