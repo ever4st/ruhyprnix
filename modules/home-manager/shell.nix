e7d754c96f112e23b2aed053bb541500497a3e73
ever4st <71986795+ever4st@users.noreply.github.com>
2026-05-15 18:35:07 +0300

    Add shell.nix for home-manager configuration

---
+++ modules/home-manager/shell.nix
@@ -0,0 +1,27 @@
+{ pkgs, ... }:
+
+{
+  programs.yazi = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+      manager = {
+        show_hidden = true;
+        sort_by = "mtime";
+      };
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "ever4pain";
+    userEmail = "your-email@example.com"; # Не забудь поменять
+  };
+
+  # Дополнительные утилиты для разработки
+  home.packages = with pkgs; [
+    fastfetch # Красивый вывод инфо о системе
+    btop      # Монитор ресурсов
+    tldr      # Короткие справки по командам
+  ];
+}
