25afffe31e19675dbbc12ab704f94edd966eb3ef
ever4st <71986795+ever4st@users.noreply.github.com>
2026-05-15 19:41:47 +0300

    Configure SDDM with Layan theme and dependencies
    Add Layan theme for SDDM with dependencies.

--- modules/system/sddm.nix
+++ modules/system/sddm.nix
@@ -1,4 +1,31 @@
-{ pkgs, ... }:
+{ pkgs, ... }: {
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+    theme = "layan";
+  };
+
+  environment.systemPackages = with pkgs; [
+    # Зависимости для темы Layan
+    libsForQt5.qt5.qtgraphicaleffects
+    libsForQt5.qt5.qtquickcontrols2
+    libsForQt5.qt5.qtsvg
+
+    (stdenv.mkDerivation {
+      name = "sddm-theme-layan";
+      src = fetchFromGitHub {
+        owner = "vinceliuice";
+        repo = "Layan-kde";
+        rev = "f56515438865463f237f37473b64c6778f6911c4"; # Актуальный коммит
+        sha256 = "sha256-4YtYIitYpYsh4N07f4Gj0D9DkC8XW8E7S6Z6G6+S6U8=";
+      };
+      installPhase = ''
+        mkdir -p $out/share/sddm/themes/layan
+        cp -R sddm/* $out/share/sddm/themes/layan
+      '';
+    })
+  ];
+}{ pkgs, ... }:
 
 {
   services.displayManager.sddm = {
