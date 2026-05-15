d4606c47e2628eed8e963e3297e000336e01f55a
ever4st <71986795+ever4st@users.noreply.github.com>
2026-05-15 19:13:27 +0300

    Update SDDM theme to Layan and adjust packages

--- modules/system/sddm.nix
+++ modules/system/sddm.nix
@@ -1,32 +1,25 @@
 { pkgs, ... }:
 
 {
-  services.displayManager = {
-    sddm = {
-      enable = true;
-      wayland.enable = true; # Включаем поддержку Wayland для SDDM
-      theme = "sugar-candy"; # Одна из самых красивых и кастомизируемых тем
-    };
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+    theme = "layan"; # Меняем на Layan
   };
 
-  environment.systemPackages = with pkgs; [
-    # Пакеты для темы SDDM
-    libsForQt5.qt5.qtgraphicaleffects
-    libsForQt5.qt5.qtquickcontrols2
-    libsForQt5.qt5.qtsvg
-    
-    # Сама тема (ставим её как системный пакет)
+  environment.systemPackages = [
+    # Кастомная сборка темы Layan для SDDM
     (pkgs.stdenv.mkDerivation {
-      name = "sddm-theme-sugar-candy";
+      name = "sddm-theme-layan";
       src = pkgs.fetchFromGitHub {
-        owner = "Kangie";
-        repo = "sddm-sugar-candy";
-        rev = "a1fae5159c8c7e44f0d8de124b14bae583edb5b8";
-        sha256 = "sha256-S6p6p9m9N5C6M6X6O6P6Q6R6S6T6U6V6W6X6Y6Z6A6B="; # Nix сам подскажет правильный хеш при ошибке
+        owner = "vinceliuice";
+        repo = "Layan-kde";
+        rev = "master"; # Или конкретный хэш для стабильности
+        sha256 = "sha256-0000000000000000000000000000000000000000000="; # Nix подскажет правильный
       };
       installPhase = ''
-        mkdir -p $out/share/sddm/themes/sugar-candy
-        cp -R . $out/share/sddm/themes/sugar-candy
+        mkdir -p $out/share/sddm/themes/layan
+        cp -R sddm/* $out/share/sddm/themes/layan
       '';
     })
   ];
