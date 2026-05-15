a7d7014c84f63ab9c71a13c56bab3d584a2ca40f
ever4st <71986795+ever4st@users.noreply.github.com>
2026-05-15 18:36:31 +0300

    Add base-devel.nix for development environment

---
+++ modules/home-manager/base-devel.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+    micro
+    neovim
+    
+    # Зависимости для компиляции и разработки (из твоего списка)
+    gcc
+    gnumake
+    cmake
+    gdb
+    rustup
+    wget
+  ];
+
+  # Настроим micro, чтобы он сразу был удобным
+  programs.micro = {
+    enable = true;
+    settings = {
+      colorscheme = "dracula";
+      tabsize = 4;
+    };
+  };
+}
