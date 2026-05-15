602e14c46e1880598df6093e2457c605a10fd320
ever4st <71986795+ever4st@users.noreply.github.com>
2026-05-15 19:31:28 +0300

    Enhance shell.nix with Zsh and Direnv settings
    Added Zsh and Direnv configuration along with shell aliases.

--- modules/home-manager/shell.nix
+++ modules/home-manager/shell.nix
@@ -1,32 +1,37 @@
-{ pkgs, ... }:
-
-{
+{ pkgs, ... }: {
   programs.yazi = {
     enable = true;
     enableZshIntegration = true;
-    settings = {
-      manager = {
-        show_hidden = true;
-        sort_by = "mtime";
-      };
+    settings.manager = {
+      show_hidden = true;
+      sort_by = "mtime";
     };
+  };
+
+  # Настроим Zsh, раз ты его выбрал основным
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    shellAliases = {
+      update = "sudo nixos-rebuild switch --flake .#no4einb";
+      conf = "yazi ~/.config/nixos/"; # Быстрый доступ к конфигам
+    };
+  };
+
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
   };
 
   programs.git = {
     enable = true;
     userName = "ever4pain";
-    userEmail = "your-email@example.com"; # Не забудь поменять
+    userEmail = "your-email@example.com";
   };
 
-  # Дополнительные утилиты для разработки
   home.packages = with pkgs; [
-    fastfetch # Красивый вывод инфо о системе
-    btop      # Монитор ресурсов
-    tldr      # Короткие справки по командам
-    ripgrep
-    fd
-    fzf
-    jq
-    direnv
+    fastfetch btop tldr ripgrep fd fzf jq
   ];
 }
